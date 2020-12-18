7585da74fcbe51587913ddd31553ff786145edcf
lorecast162 <lorecastcauli@gmail.com>
2020-12-18 20:43:20 +0100

    have triangle's colors be determined by coordinates

--- main.c
+++ main.c
@@ -155,7 +155,7 @@ unsigned int compileFragmentShader() {
 	const char *source = "#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"void main(){\n"
-		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"	FragColor = vec4(1.0f*(gl_FragCoord.x / 800), 1.0f*(gl_FragCoord.y / 600), 1.0f*gl_FragCoord.z, 0.0f);\n"
 		"}";
 
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
