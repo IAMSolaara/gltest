3b055808f37369467806f3e8584fe97eebd6e697
lorecast162 <lorecastcauli@gmail.com>
2020-12-18 20:42:36 +0100

    got triangle to draw with simple shaders

--- main.c
+++ main.c
@@ -5,16 +5,25 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define WINDOW_TITLE "LearnOpenGL"
+
+//vertices for our lil coote triangle👉👈
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f,
+};
 
 //callbacks
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 //custom funcs
 void inputHandler(GLFWwindow* window);
+unsigned int compileVertexShader();
+unsigned int compileFragmentShader();
+unsigned int createShaderProgram();
 
 int main() {
 	//initialize glfw
@@ -55,6 +64,29 @@ int main() {
 	//set resize callback to handle window resize
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//generate vertex buffer object and vertex array object
+	unsigned int VBO;
+	unsigned int VAO;
+
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	//bind VAO to vertex arrays.
+	glBindVertexArray(VAO);
+
+	//bind VBO buffer to the GL_ARRAY_BUFFER, which we'll use as vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//transfer verts array into VBO buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//set vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//create shader program to render poly
+	unsigned int shaderProgram = createShaderProgram();
+
 	//main render loop
 	while (!glfwWindowShouldClose(window)) {
 		//clear to dark aqua-ish color
@@ -66,6 +98,11 @@ int main() {
 		
 		//handle input events
 		inputHandler(window);
+
+		//draw triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
 		//swap buffers to present to screen
 		glfwSwapBuffers(window);
@@ -84,4 +121,78 @@ void inputHandler(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, 1);
 	}
+}
+
+//wrapper function to handle compilation of vertex shader.
+//done this way to "keep main code clean".
+unsigned int compileVertexShader() {
+	const char *source = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main(){\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+		"}";
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &source, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		fprintf(stderr, "ERROR::SHADER::VERTEX::COMPILATION_FAILED:\n%s\n", infoLog);
+	}
+
+	return vertexShader;
+}
+
+//wrapper function to handle compilation of fragment shader.
+//done this way to "keep main code clean".
+unsigned int compileFragmentShader() {
+	const char *source = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main(){\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}";
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, &source, NULL);
+	glCompileShader(fragmentShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		fprintf(stderr, "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED:\n%s\n", infoLog);
+	}
+
+	return fragmentShader;
+}
+
+//wrapper function to create our shader program
+unsigned int createShaderProgram() {
+	unsigned int vertexShader = compileVertexShader();
+	unsigned int fragmentShader = compileFragmentShader();
+
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int success; char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		fprintf(stderr, "GL Program Link Error:\n%s\n", infoLog);
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
 }
